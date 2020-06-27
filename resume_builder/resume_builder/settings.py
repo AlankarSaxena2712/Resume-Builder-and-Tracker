7ce8c1219a390887502c888c66f87e3a43f63bb0
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2020-06-27 14:00:59 +0530

    Added gitignore

--- resume_builder/resume_builder/settings.py
+++ resume_builder/resume_builder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sdr%$k6bj#1v)v1)-00+g7o5b5meckcmikujx0l9ira#ww@n2r'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
